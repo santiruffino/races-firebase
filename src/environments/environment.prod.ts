18e9f2027503dcb6a28916bfaef3e0c82b5f1b06
Santi Ruffino <santiagoruffino@gmail.com>
2022-05-17 21:12:39 -0300

    Env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: process.env["API_KEY"],
+    apiKey: process.env["NG_APP_API_KEY"],
     authDomain: "races-app-534ee.firebaseapp.com",
-    databaseURL: process.env["DATABASE_URL"],
+    databaseURL: process.env["NG_APP_DATABASE_URL"],
     projectId: "races-app-534ee",
     storageBucket: "races-app-534ee.appspot.com",
-    messagingSenderId: process.env["MESSAGING_SENDER_ID"],
+    messagingSenderId: process.env["NG_APP_MESSAGING_SENDER_ID"],
     appId: "1:891836155528:web:90cfc8a8d82fd79b1c013d",
-    measurementId: process.env["MEASUREMENT_ID"]
+    measurementId: process.env["NG_APP_MEASUREMENT_ID"]
   }
 };
